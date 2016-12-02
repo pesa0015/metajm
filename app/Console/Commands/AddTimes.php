9f53bf315b43ec98e30af0aba1edad8e4bd27168
pesa0015 <pesa0015@gmail.com>
2016-12-02 21:25:56 +0000

    Save available minutes

--- app/Console/Commands/AddTimes.php
+++ app/Console/Commands/AddTimes.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Http\Requests;
 use App\Time;
+use App\TimeLeft;
 use App\companies_employers;
 use DB;
 use DateTime;
@@ -44,20 +45,24 @@ class AddTimes extends Command
      */
     public function handle()
     {
-        $employers = DB::table('times')
-                        ->join('companies_employers', 'times.employer_id', '=', 'companies_employers.id')
-                        ->select(DB::raw("times.employer_id AS id, companies_employers.repeat_weeks * 7 - (DATEDIFF(MAX(timestamp), CURDATE())) AS days_left, MAX(timestamp) AS last_day, companies_employers.repeat_weeks AS weeks, default_opening_hours"))
+        $employers = DB::table('time_left')
+                        ->join('companies_employers', 'time_left.employer_id', '=', 'companies_employers.id')
+                        ->select(DB::raw("time_left.employer_id AS id, time_left.company_id, companies_employers.repeat_weeks * 7 - (DATEDIFF(MAX(close), CURDATE())) AS days_left, MAX(close) AS last_day, companies_employers.repeat_weeks AS weeks, default_opening_hours"))
                         ->groupBy('companies_employers.id')
-                        ->orderBy('times.id')->get();
+                        ->orderBy('time_left.id')->get();
         
-        function getDayTimes($day, $hour, $employer_id) {
+        function getDayTimes($day, $hour, $employer_id, $company_id) {
+            $beginOriginal = new DateTime($day->format('Y-m-d') . ' ' . explode('-', $hour)[0]);
             $begin = new DateTime($day->format('Y-m-d') . ' ' . explode('-', $hour)[0]);
             $end = new DateTime($day->format('Y-m-d') . ' ' . explode('-', $hour)[1]);
 
             $hourDiff = $begin->diff($end);
+            $minutes_open = $hourDiff->h * 60;
 
-            if ($hourDiff->i == 30)
+            if ($hourDiff->i == 30) {
                 $hourDiff->h++;
+                $minutes_open += 30;
+            }
 
             $array = array();
 
@@ -65,7 +70,7 @@ class AddTimes extends Command
                 array_push($array,array('timestamp' => $begin->format('Y-m-d H:i'), 'employer_id' => $employer_id));
                 $begin = $begin->modify('+30 minutes');
             }
-            return $array;
+            return array('array' => $array, 'minutes_left' => array('start' => $beginOriginal->format('Y-m-d H:i'), 'close' => $end->format('Y-m-d H:i'), 'max_available_minutes' => $minutes_open, 'company_id' => $company_id, 'employer_id' => $employer_id));
         }
 
         function checkDay($d) {
@@ -73,9 +78,6 @@ class AddTimes extends Command
         }
 
         foreach($employers as $employer) {
-            // if ($employer->days_left > 7)
-                // continue;
-
             $begin = new DateTime($employer->last_day);
             $begin = $begin->modify('+1 day');
             $end = new DateTime($employer->last_day);
@@ -96,45 +98,59 @@ class AddTimes extends Command
             
             $hours = array();
 
+            $minutes_left = array();
+
             foreach($period as $dt) {
-                $dayOfWeek = date('l', strtotime($dt->format('Y-m-d')));
+                $dayOfWeek = date('N', strtotime($dt->format('Y-m-d')));
                 
-                if ($dayOfWeek === 'Monday' && $monday) {
-                    $today = getDayTimes($dt,$day->mon,$employer->id);
-                    array_push($hours, $today);
+                if ($dayOfWeek == 1 && $monday) {
+                    $today = getDayTimes($dt,$day->mon,$employer->id,$employer->company_id);
+                    array_push($hours, $today['array']);
+                    array_push($minutes_left, $today['minutes_left']);
                 }
-                if ($dayOfWeek === 'Tuesday' && $tuesday) {
-                    $today = getDayTimes($dt,$day->tue,$employer->id);
-                    array_push($hours, $today);
+                if ($dayOfWeek == 2 && $tuesday) {
+                    $today = getDayTimes($dt,$day->tue,$employer->id,$employer->company_id);
+                    array_push($hours, $today['array']);
+                    array_push($minutes_left, $today['minutes_left']);
                 }
-                if ($dayOfWeek === 'Wednesday' && $wednesday) {
-                    $today = getDayTimes($dt,$day->wed,$employer->id);
-                    array_push($hours, $today);
+                if ($dayOfWeek == 3 && $wednesday) {
+                    $today = getDayTimes($dt,$day->wed,$employer->id,$employer->company_id);
+                    array_push($hours, $today['array']);
+                    array_push($minutes_left, $today['minutes_left']);
                 }
-                if ($dayOfWeek === 'Thursday' && $thursday) {
-                    $today = getDayTimes($dt,$day->thu,$employer->id);
-                    array_push($hours, $today);
+                if ($dayOfWeek == 4 && $thursday) {
+                    $today = getDayTimes($dt,$day->thu,$employer->id,$employer->company_id);
+                    array_push($hours, $today['array']);
+                    array_push($minutes_left, $today['minutes_left']);
                 }
-                if ($dayOfWeek === 'Friday' && $friday) {
-                    $today = getDayTimes($dt,$day->fri,$employer->id);
-                    array_push($hours, $today);
+                if ($dayOfWeek == 5 && $friday) {
+                    $today = getDayTimes($dt,$day->fri,$employer->id,$employer->company_id);
+                    array_push($hours, $today['array']);
+                    array_push($minutes_left, $today['minutes_left']);
                 }
-                if ($dayOfWeek === 'Saturday' && $saturday) {
-                    $today = getDayTimes($dt,$day->sat,$employer->id);
-                    array_push($hours, $today);
+                if ($dayOfWeek == 6 && $saturday) {
+                    $today = getDayTimes($dt,$day->sat,$employer->id,$employer->company_id);
+                    array_push($hours, $today['array']);
+                    array_push($minutes_left, $today['minutes_left']);
                 }
-                if ($dayOfWeek === 'Sunday' && $sunday) {
-                    $today = getDayTimes($dt,$day->sun,$employer->id);
-                    array_push($hours, $today);
+                if ($dayOfWeek == 7 && $sunday) {
+                    $today = getDayTimes($dt,$day->sun,$employer->id,$employer->company_id);
+                    array_push($hours, $today['array']);
+                    array_push($minutes_left, $today['minutes_left']);
                 }
             }
             $myHours = array();
+            $myMinutes = array();
             foreach ($hours as $hour) {
                 foreach ($hour as $currentHour) {
                     array_push($myHours, $currentHour);
                 }
             }
+            foreach ($minutes_left as $minutes) {
+                array_push($myMinutes, $minutes);
+            }
             Time::insert($myHours);
+            TimeLeft::insert($myMinutes);
         }
     }
 }
