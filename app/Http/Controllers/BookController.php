60632b95bcc99fd73648f25fa9c726d90d332631
pesa0015 <pesa0015@gmail.com>
2016-12-05 15:02:51 +0000

    Svea payment-integration

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Booking;
+use App\companies;
+use App\TimeLeft;
+use App\Time;
+use App\Category;
+use App\Service;
+use Auth;
+use DB;
+use DateTime;
+
+require 'Svea/Includes.php';
+
+class BookController extends Controller
+{
+    public function goToSveaPaymentPage(Request $request)
+    {
+        $this->validate($request, [
+            'company' => 'required|numeric',
+            'service' => 'required|numeric',
+        ]);
+
+        $companyId = $request->company;
+        $serviceId = $request->service;
+
+        \Config::set('auth.defaults.guard', 'users');
+
+        if (!Auth::check()) 
+            return response()->json(['success' => false]);
+
+        $company = companies::find($companyId);
+        $service = Service::with('category')->find($serviceId);
+        $price = (float) $service->price;
+
+        $start = $request->time;
+
+        $today = new DateTime($start);
+        $end = new DateTime($start);
+        $end = $end->modify("+ {$service->time} min");
+
+        $booking = new Booking;
+        $booking->booked_at = \Carbon\Carbon::now();
+        $booking->start = $start;
+        $booking->end = $end;
+        $booking->payment = 0;
+        $booking->booked_by_user = Auth::user()->id;
+        $booking->service_id = $service->id;
+        $booking->company_id = $companyId;
+        $booking->employer_id = 1;
+        $booking->save();
+
+        Time::where('timestamp', '>=', $today->format('Y-m-d H:i'))->where('timestamp', '<', $end->format('Y-m-d H:i'))->update(['booking_id' => $booking->id]);
+
+        // get config object
+        $myConfig = \Svea\SveaConfig::getTestConfig(); //replace with class holding your merchantid, secretword, et al, adopted from package Config/SveaConfig.php
+        // We assume that you've collected the following information about the order in your shop:
+        // customer information:
+        $customerFirstName = Auth::user()->first_name;
+        $customerLastName = Auth::user()->last_name;
+        $customerAddress = Auth::user()->address;
+        $customerZipCode = Auth::user()->postal_code;
+        $customerCity = Auth::user()->city;
+        $customerCountry = Auth::user()->country;
+        // The customer has bought three items, one "Billy" which cost 700,99 kr excluding vat (25%) and two hotdogs for 5 kr (incl. vat).
+        // We'll also need information about the customer country, and the currency used for this order, etc., see below
+        // Start the order creation process by creating the order builder object by calling WebPay::createOrder():
+        $myOrder = \WebPay::createOrder( $myConfig );
+        // You then add information to the order object by using the methods in the Svea\CreateOrderBuilder class.
+        // For a Card order, the following methods are required:
+        $myOrder->setCountryCode("SE");                         // customer country, we recommend basing this on the customer billing address
+        $myOrder->setCurrency("SEK");                           // order currency
+        $myOrder->setClientOrderNumber( "order #".date('c') );  // required - use a not previously sent client side order identifier, i.e. "order #20140519-371"
+        // You may also chain fluent methods together:
+        $myOrder
+                ->setCustomerReference("customer #123")         // optional - This should contain a customer reference, as in "customer #123".
+                ->setOrderDate(date('c'))                    // optional - or use an ISO8601 date as produced by i.e. date('c')
+        ;
+        // Then specify the items bought as order rows, using the methods in the Svea\OrderRow class, and adding them to the order:
+        $firstBoughtItem = \WebPayItem::orderRow();
+        $firstBoughtItem->setAmountExVat( $price );
+        $firstBoughtItem->setVatPercent( 25 );
+        $firstBoughtItem->setQuantity( 1 );
+        $firstBoughtItem->setDescription( "Bokning: " . $company->name );
+        $firstBoughtItem->setArticleNumber("ServiceID:" . $service->id);
+        // Add firstBoughtItem to order row
+        $myOrder->addOrderRow( $firstBoughtItem );
+        // For card orders the ->addCustomerDetails() method is optional, but recommended, so we'll add what info we have
+        $myCustomerInformation = \WebPayItem::individualCustomer(); // there's also a ::companyCustomer() method, used for non-person entities
+        // Set customer information, using the methods from the IndividualCustomer class
+        $myCustomerInformation->setName( $customerFirstName, $customerLastName);
+        $sveaAddress = \Svea\Helper::splitStreetAddress($customerAddress); // Svea requires an address and a house number
+        $myCustomerInformation->setStreetAddress( $sveaAddress[0], $sveaAddress[1] );
+        $myCustomerInformation->setZipCode( $customerZipCode )->setLocality( $customerCity );
+        $myOrder->addCustomerDetails( $myCustomerInformation );
+        // We have now completed specifying the order, and wish to send the payment request to Svea. To do so, we first select a payment method.
+        // For card orders, we recommend using the ->usePaymentMethod(PaymentMethod::SVEACARDPAY).
+        $myCardOrderRequest = $myOrder->usePaymentMethod(\PaymentMethod::SVEACARDPAY);
+        // Then set any additional required request attributes as detailed below. (See Svea\PaymentMethodPayment and Svea\HostedPayment classes for details.)
+        $myCardOrderRequest
+            ->setCardPageLanguage("SV")                                     // ISO639 language code, i.e. "SV", "EN" etc. Defaults to English.
+            ->setReturnUrl('http://metajm.se/booking/done'); // The return url where we receive and process the finished request response
+        // Get a payment form object which you can use to send the payment request to Svea
+        $myCardOrderPaymentForm = $myCardOrderRequest->getPaymentForm();
+
+        $url = 'https://test.sveaekonomi.se/webpay/payment';
+        $message = $myCardOrderPaymentForm->rawFields['message'];
+        $mac = $myCardOrderPaymentForm->rawFields['mac'];
+
+        $form = '<form name="paymentForm" id="paymentForm" method="post" action="' . $url . '">'
+                        . '<input type="hidden" id="merchantid" name="merchantid" value="1200">'
+                        . '<input type="hidden" id="message" name="message" value="' . $message . '">'
+                        . '<input type="hidden" id="mac" name="mac" value="' . $mac . '">'
+                        . '<input type="hidden" id="booking" name="booking" value="' . $booking->id . '">'
+                        . '<input type="submit" name="btnSubmit">'
+                    . '</form>';
+
+        return response()->json(['success' => true, 'go_to_payment' => $form])->withCookie(cookie('booking', $booking->id, 15));
+    }
+
+    public function start(Request $request)
+    {
+        \Config::set('auth.defaults.guard', 'users');
+
+        if (Auth::check()) {
+            return response()->json(['is_logged_in' => true, 'user' => Auth::user()]);
+        }
+
+        return response()->json(['is_logged_in' => false]);
+    }
+}
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
+use Illuminate\Http\Request;
+use Auth;
+use DateTime;
+
+require 'Svea/Includes.php';
 
 class IndexController extends Controller
 {
+
     public function welcome()
     {
     	return view('index');
+    }
+
+    public function check(Request $request)
+    {
+    	// get config object
+        $myConfig = \Svea\SveaConfig::getTestConfig();
+        $countryCode = 'SE'; // should match request countryCode
+        // the raw request response is posted to the returnurl (this page) from Svea.
+        $rawResponse = $_POST;
+        // decode the raw response by passing it through the Svea\WebPay\Response\SveaResponse class
+        $myResponse = new \SveaResponse( $rawResponse, $countryCode, $myConfig );
+        // The decoded response is available through the ->getResponse() method.
+        // Check the response attribute 'accepted' for true to see if the request succeeded, if not, see the attributes resultcode and/or errormessage
+        if ($myResponse->getResponse()->accepted == 1) {
+            $booking_id = \Crypt::decrypt(\Cookie::get('booking'));
+            $booking = \App\Booking::with('company')->find($booking_id);
+
+            $booking->payment = 1;
+            $booking->transaction_id = $myResponse->getResponse()->transactionId;
+            $booking->payment_method = $myResponse->getResponse()->paymentMethod;
+            $booking->booking_key = \Cookie::get('booking');          
+            $booking->save();
+                                
+            return view('success', ['time' => new DateTime($booking->start), 'company' => $booking->company]);
+        }
+        else {
+            return redirect('/');
+        }
     }
 }
