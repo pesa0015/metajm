aeea0905346fd0834fc848981d555c2d3e0cf0f3
pesa0015 <pesa0015@gmail.com>
2017-06-18 08:28:02 +0000

    Remove imports in User.php and Stylist.php

--- app/Stylist.php
+++ app/Stylist.php
@@ -2,21 +2,10 @@
 
 namespace App;
 
-// use Illuminate\Auth\Authenticatable as Authenticatable;
-
-// use Illuminate\Database\Eloquent\Model as Model;
-// use Illuminate\Foundation\Auth\User as Authenticatable;
-
-use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class Stylist extends Model implements AuthenticatableContract, CanResetPasswordContract
+class Stylist extends Model
 {
-    use Authenticatable, CanResetPassword;
-
     protected $fillable = [
         'first_name',
         'last_name',
